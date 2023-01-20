b3fa40fe496f8759484dc1550072522f9062fdfb
*zzz <102775197+YESGREEN1@users.noreply.github.com>
2023-01-20 13:28:27 +0900

    Add files via upload

---
+++ parachute.lua
@@ -0,0 +1,95 @@
+require"natives-1651208000"
+
+floatt=0
+      self11 = PLAYER.PLAYER_PED_ID()
+      selfpos = ENTITY.GET_ENTITY_COORDS(self11,true)
+
+
+      menu.slider(menu.my_root(),"Speed: [PARACHUTE] ",{"speedcom"},"",0,1000000,100,100,function(value)
+       floatt=value
+      end)
+
+ menu.toggle(menu.my_root(),"PARACHUTE",{},"keyboard=Z\nController=R3",function()
+
+  while true do
+    util.yield()    
+ if ENTITY.IS_ENTITY_IN_AIR(PLAYER.PLAYER_PED_ID()) then
+ TASK.SET_PARACHUTE_TASK_THRUST(PLAYER.PLAYER_PED_ID(),floatt)
+   
+  if PAD.IS_CONTROL_PRESSED(0,20) or PAD.IS_CONTROL_PRESSED(0,R3)  then
+   ENTITY.APPLY_FORCE_TO_ENTITY(PLAYER.PLAYER_PED_ID(), 1, 0, 1, 10, 0, 0, 0, 1, 1, 1, 1, 0, 1)
+   TASK.TASK_PARACHUTE_TO_TARGET(self11,selfpos.x,selfpos.y,selfpos.z)
+  end 
+ end
+end
+  end)
+
+
+ragdoll_type=0
+
+function control()
+if PAD.IS_CONTROL_PRESSED(0,9)  then--D
+  ENTITY.APPLY_FORCE_TO_ENTITY(PLAYER.PLAYER_PED_ID(), 1, 1, 0, 1, 0, 0, 0, 1,true,true,true,false,true)
+elseif  PAD.IS_CONTROL_PRESSED(0,34) then --A
+  ENTITY.APPLY_FORCE_TO_ENTITY(PLAYER.PLAYER_PED_ID(), 1, -1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1)
+elseif  PAD.IS_CONTROL_PRESSED(0,32) then--W
+  ENTITY.APPLY_FORCE_TO_ENTITY(PLAYER.PLAYER_PED_ID(), 1, 0, 1, 1, 0, 0, 4, 1, 1, 1, 1, 0, 1) 
+elseif  PAD.IS_CONTROL_PRESSED(0,31) then--S
+  ENTITY.APPLY_FORCE_TO_ENTITY(PLAYER.PLAYER_PED_ID(), 1, 0, -1, -1, 0, 0, -4, 1, 1, 1, 1, 0, 1) 
+elseif PAD.IS_CONTROL_PRESSED(0,61)  then --shi
+  ENTITY.APPLY_FORCE_TO_ENTITY(PLAYER.PLAYER_PED_ID(), 1, 0, 0, 1, 1, 1, -10, 1, 1, 1, 1, 0, 1) 
+ end
+end
+
+
+menu.slider(menu.my_root(),"ragdoll type: [force ragdoll] ",{"ragty"},"",0,3,1,1,function(value_two)
+  ragdoll_type=value_two
+ end)
+
+ menu.toggle_loop(menu.my_root(),"force ragdoll",{},"",function(on)
+
+
+
+  util.toast("Gracefulness off")
+  menu.trigger_commands("grace off")
+  control()
+  PED.SET_PED_TO_RAGDOLL(PLAYER.PLAYER_PED_ID(), 2000, 2000, ragdoll_type,true , true , true )
+
+
+
+
+ end)
+
+--[[ old code
+ menu.toggle_loop(menu.my_root(),"force ragdoll",{},"Z=on\nE=off",function(x)
+    x = true 
+ 
+  if PAD.IS_CONTROL_PRESSED(0,20)then
+    util.toast("Gracefulness off")
+      while x==true do
+    menu.trigger_commands("grace off")
+   PED.SET_PED_TO_RAGDOLL(PLAYER.PLAYER_PED_ID(), 2000, 2000, ragdoll_type,true , true , true )
+     ENTITY.APPLY_FORCE_TO_ENTITY(PLAYER.PLAYER_PED_ID(), 1, 0, 1, 10, 0, -5, 0, 1, 1, 1, 1, 0, 1) do 
+ util.yield(20)
+ ENTITY.APPLY_FORCE_TO_ENTITY(PLAYER.PLAYER_PED_ID(), 1, 0, 1, 1, 0, -2, 0, 1, 1, 1, 1, 0, 1)  
+     util.yield()
+          if 
+    PAD.IS_CONTROL_PRESSED(0,38) 
+         then
+       x=false  
+       menu.trigger_commands("grace on")
+        util.toast("Gracefulness on")
+    
+    end
+    end
+  end
+ end
+end)
+--]]
+ 
+ menu.hyperlink(menu.my_root(), "join discord", "https://discord.gg/UAkyMGQeYd", "")
+ util.keep_running()
+
+
+
+
